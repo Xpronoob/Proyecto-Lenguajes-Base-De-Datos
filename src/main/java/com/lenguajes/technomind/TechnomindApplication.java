7278659f15196a81a8dfe6f1fccecba9946230e5
Xpronoob <kevin_03_puris@hotmail.com>
2023-07-04 11:10:45 -0600

    init

---
+++ src/main/java/com/lenguajes/technomind/TechnomindApplication.java
@@ -0,0 +1,13 @@
+package com.lenguajes.technomind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnomindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnomindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lenguajes/technomind/TechnomindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lenguajes.technomind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnomindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
